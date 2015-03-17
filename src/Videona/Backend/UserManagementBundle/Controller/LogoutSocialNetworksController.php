397e84a2dab886725977537f19185c18a000ccff
Verónica Lago <verofomi@gmail.com>
2015-03-17 16:16:23 +0100

    Implementing logout interface

---
+++ src/Videona/Backend/UserManagementBundle/Controller/LogoutSocialNetworksController.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the Videona code project.
+ * 
+ * Copyright (C) 2015 Videona Socialmedia SL
+ * http://www.videona.com
+ * info@videona.com
+ *
+ * All rights reserved
+ */
+
+namespace Videona\Backend\UserManagementBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * LogoutSocialNetworksController is a class that allows users to logout from
+ * Videona and other social networks if user has logged with them
+ *
+ * @author vlf
+ */
+class LogoutSocialNetworksController extends Controller {
+
+    /**
+     * Method to logout a user from Videona.
+     * 
+     * @param Request $request
+     * 
+     * @return view
+     */
+    public function logoutSocialAction(Request $request) {
+        // Show logout button
+        return $this->render('VideonaBackendUserManagementBundle:LogoutSocialNetworks:logout.html.twig');
+    }
+
+}
